ce67248a8aa9dac6cc4396e79a49fcade467da26
tinashe <karimugwagwanorkias@gmail.com>
2022-10-18 21:07:28 +0200

    added Input component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Input from './Input'
 
 function Calculator() {
+
   return (
     <>
         <h2>hello world!!!</h2>
+        <Input/>
+        
     </>
   )
 }
---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <>
+    <h2>hello people</h2>
+    </>
+  )
+}
+
+export default Input
