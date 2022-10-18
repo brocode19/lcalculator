8e50d2b0593f1612dc36d6fd0dfed42674823e00
tinashe <karimugwagwanorkias@gmail.com>
2022-10-19 00:50:43 +0200

    added space between the submit buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Doughnut, Pie } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 import 'chart.js/auto';
 import Input from './Input'
 import Loan from "./Loan";
